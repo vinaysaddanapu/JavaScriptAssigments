cc040f2de718987c5f49ecb12b9e67af5a8f577f
vinaysaddanapu <vinay.saddanapu@gmail.com>
2020-03-11 09:16:14 +0530

    JavaScipt Assignments

---
+++ Assignment 3/DemoFunction.js
@@ -0,0 +1,14 @@
+function goodEveningNeighbour(){
+ 
+    return 'Good Evening, neighbour thanks for everything !';
+
+
+}
+
+function goodMorningNeighbour(name){
+
+    return  'Good Morning, ' + name + '!';
+}
+
+console.log(goodEveningNeighbour());
+console.log(goodMorningNeighbour('Joe'));
